1372fb8a081f7208561c31e3ac7685b0b6fcd952
JulioVianaDev <JulioVianaDev@gmail.com>
2025-07-04 19:02:51 -0300

    corrigindo o react router para dentro do provider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,40 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Inicial } from './Components/pagInicial';
+import { Solicitacoes } from './Components/Solicitacoes';
+import { SolsPref } from './Components/SolsPref';
+import { NewSol } from './Components/NewSol';
+import Edit from './Components/EditPref';
+import { LoginPrefeitura } from './Components/LoginPrefeitura';
+import { LoginPessoa } from './Components/LoginPessoa';
+import { Profile } from './Components/Profile';
+import { ProfileCivil } from './Components/ProfileCivil';
+import { Config } from './Components/Config';
+import { Proposito } from './Components/ConfigPages/proposito';
+import { Ajuda } from './Components/ConfigPages/Ajuda';
+import { About } from './Components/ConfigPages/About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>                    
+            <Routes> 
+                <Route path="/" element={<Inicial/>}/>
+                 <Route path="/solscivil" element={<Solicitacoes/>}/>
+                 <Route path="/solspref" element={<SolsPref/>}/>
+                 <Route path="/newsol" element={<NewSol/>}/>
+                 <Route path="/edit/:id" element={<Edit/>}/>
+                 <Route path="/loginpref" element={<LoginPrefeitura/>}/>
+                 <Route path="/logincivil" element={<LoginPessoa/>}/>
+                 <Route path="/profilepref" element={<Profile/>}/>
+                 <Route path="/profilecivil" element={<ProfileCivil/>}/>
+                 <Route path="/config" element={<Config/>}/>
+                 <Route path='/config/proposito' element={<Proposito/>}/>
+                 <Route path='/config/help' element={<Ajuda/>}/>
+                 <Route path='/config/about' element={<About/>}/>
+            </Routes>    
+            </BrowserRouter>    
   </React.StrictMode>,
   document.getElementById('root')
 );
